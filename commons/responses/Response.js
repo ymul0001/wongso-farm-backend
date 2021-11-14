88dd3248779531338f00d8c7d9380760414d3d33
Yonas Mulyadi <ymul0001@student.monash.edu>
2021-11-14 09:40:46 +0700

    add another response function for paginated data

--- commons/responses/Response.js
+++ commons/responses/Response.js
@@ -3,10 +3,21 @@
 const returnResponse = (res, status, message) => {
     return res.status(status).json({
         status: status,
+        ...(message.constructor === Array && {totalCount: message[0].length}),
         message: (message.constructor === Array) ? message[0] : message
     })
 }
 
+const returnPaginatedResponse = (res, status, message, totalCount, pageCount) => {
+    return res.status(status).json({
+        status: status,
+        totalAvailableData: totalCount,
+        totalCurrentPageData: pageCount,
+        message: message[0]
+    })
+}
+
 module.exports =  {
-    returnResponse
+    returnResponse,
+    returnPaginatedResponse
 }
