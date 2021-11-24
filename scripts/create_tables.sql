e891be18550fdd6f28b2f26bb73c78934faed858
Yonas Mulyadi <ymul0001@student.monash.edu>
2021-11-24 11:50:26 +0700

    add create table statements and dump files

--- scripts/create_tables.sql
+++ scripts/create_tables.sql
@@ -14,8 +14,27 @@ CREATE TABLE `wongso`.`expense` (
 `date_created` DATETIME NULL,
 PRIMARY KEY (`expense_id`));
 
+CREATE TABLE `wongso`.`customer` (
+  `customer_id` VARCHAR(255) NOT NULL,
+  `customer_initial` VARCHAR(45) NOT NULL,
+  `customer_name` VARCHAR(100) NOT NULL,
+  `customer_address` VARCHAR(255),
+  `date_created` DATETIME NULL,
+  PRIMARY KEY (`customer_id`));
+
+CREATE TABLE `wongso`.`usercustomer` (
+  `user_id` VARCHAR(255) NOT NULL,
+  `customer_id` VARCHAR(255) NOT NULL,
+  PRIMARY KEY (`user_id`, `customer_id`));
+
 /*
   ALTER TABLE TO ADD FOREIGN KEY CONSTRAINTS
 */
 ALTER TABLE wongso.expense
 ADD CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES wongso.user(user_id);
+
+ALTER TABLE wongso.usercustomer
+ADD CONSTRAINT fk_user_id_customer_table FOREIGN KEY (user_id) REFERENCES wongso.user(user_id);
+
+ALTER TABLE wongso.usercustomer
+ADD CONSTRAINT fk_customer_id_customer_table FOREIGN KEY (customer_id) REFERENCES wongso.customer(customer_id);
