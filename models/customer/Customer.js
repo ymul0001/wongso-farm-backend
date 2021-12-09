aebe48aa288347df4758c13a3cc0b4c0b1b26566
Yonas Mulyadi <ymul0001@student.monash.edu>
2021-12-10 00:15:31 +0700

    fix cors

--- models/customer/Customer.js
+++ models/customer/Customer.js
@@ -19,15 +19,15 @@ const findCustomerByUserIdSorted = (userId, sortKey, sortOrder) => {
                                  SELECT c.customer_id, c.customer_initial, c.customer_name, c.customer_address
                                  FROM customer c 
                                  JOIN usercustomer uc ON c.customer_id = uc.customer_id
-                                 WHERE uc.user_id = 'c58665af-1318-4a31-b11a-0f2b810eb3ac'
+                                 WHERE uc.user_id = '${userId}'
                                  ) T1 
                                  LEFT JOIN 
                                  (SELECT c.customer_id, SUM(s.qty) as total_order, SUM(s.total_price) as total_price 
                                  FROM customer c 
                                  JOIN sales s ON c.customer_id = s.customer_id
-                                 WHERE s.user_id = 'c58665af-1318-4a31-b11a-0f2b810eb3ac' AND s.level = 'piece'
+                                 WHERE s.user_id = '${userId}' AND s.level = 'piece'
                                  GROUP BY c.customer_id) T2 ON T1.customer_id = T2.customer_id
-                                 ORDER BY total_order DESC`);
+                                 ORDER BY ${sortKey} ${sortOrder}`);
 }
 
 const createCustomer = (params) => {
