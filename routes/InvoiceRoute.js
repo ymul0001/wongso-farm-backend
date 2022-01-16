b3c512090d996febfaa4e1281224d3e24c229ba2
Yonas Mulyadi <ymul0001@student.monash.edu>
2022-01-16 15:53:30 +1100

    add get sales and customer names for the invoice features

--- routes/InvoiceRoute.js
+++ routes/InvoiceRoute.js
@@ -4,7 +4,12 @@
 const express = require('express');
 const router = express.Router();
 const listInvoiceItemsController = require('../controllers/InvoiceController/ListInvoiceItemsController');
+const listDistinctCustomerNamesWithSalesController = require('../controllers/InvoiceController/ListDistinctCustomerNamesWithSalesController');
+const listSalesDateForEachCustomerController = require('../controllers/InvoiceController/ListSalesDateForEachCustomerController');
 
 router.get(`/findInvoiceItems`, listInvoiceItemsController.listInvoiceItems);
+router.get( `/findDistinctCustomerNames`, listDistinctCustomerNamesWithSalesController.listCustomerNames);
+router.get(`/findSalesDateForEachCustomerName`, listSalesDateForEachCustomerController.listSalesDate);
+
 
 module.exports = router;
